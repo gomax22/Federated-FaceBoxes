ec8ceebfa00746a4e00af269d6f69869cacbe82d
gomax22 <giordanoorsini.massimiliano@gmail.com>
2024-06-07 16:54:06 +0200

    moving towards app example (adap/flower/examples/app-pytorch)

--- server.py
+++ server.py
@@ -3,10 +3,10 @@ from collections import OrderedDict
 import argparse
 import flwr as fl
 import torch
-from helper import load_faceboxes
+from task import load_faceboxes, get_weights
 import warnings
 from typing import List, Union, Optional
-from flwr.common import Parameters, Scalar, Metrics
+from flwr.common import Parameters, Scalar, Metrics, ndarrays_to_parameters
 import numpy as np
 import os
 from pathlib import Path
@@ -99,52 +99,66 @@ def evaluate_weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
             "regr_loss": sum(losses_l) / sum(examples),
             "class_loss": sum(losses_c) / sum(examples)}
 
-def main():
-    """Load model for server-side parameter initialization"""
+# Parse command line argument `partition`
+parser = argparse.ArgumentParser(description="Flower")
+parser.add_argument("--toy", action="store_true", 
+                    help="Set to true to use only 10 datasamples for validation. \
+                        Useful for testing purposes. Default: False")
+parser.add_argument("--num_rounds", default=4, type=int, help="Number of rounds of federated learning. Default: 4")
+parser.add_argument("--server_address", default="0.0.0.0:8080", type=str, help="Server address. Default: 0.0.0.0:8080")
+parser.add_argument("--num_clients", default=2, type=int, help="Number of clients. Default: 2")
+parser.add_argument("--resume_net", default=None, type=str, help="Path to resume network for retraining. Default: None")
+parser.add_argument("--mode", type=str, default="train", choices=["train", "test"], help="Mode of the model. Default: train")
+parser.add_argument("--img_dim", type=int, default=1024, help="Image dimension. Default: 1024")
+parser.add_argument("--num_classes", type=int, default=2, help="Number of classes. Default: 2")    
+parser.add_argument("--weights_dir", type=str, default="./weights", help="Directory to save model weights. Default: ./weights")
+args = parser.parse_args()
 
-    # Parse command line argument `partition`
-    parser = argparse.ArgumentParser(description="Flower")
-    parser.add_argument("--toy", action="store_true", 
-                        help="Set to true to use only 10 datasamples for validation. \
-                            Useful for testing purposes. Default: False")
-    parser.add_argument("--num_rounds", default=4, type=int, help="Number of rounds of federated learning. Default: 4")
-    parser.add_argument("--server_address", default="0.0.0.0:8080", type=str, help="Server address. Default: 0.0.0.0:8080")
-    parser.add_argument("--num_clients", default=2, type=int, help="Number of clients. Default: 2")
-    parser.add_argument("--resume_net", default=None, type=str, help="Path to resume network for retraining. Default: None")
-    parser.add_argument("--mode", type=str, default="train", choices=["train", "test"], help="Mode of the model. Default: train")
-    parser.add_argument("--img_dim", type=int, default=1024, help="Image dimension. Default: 1024")
-    parser.add_argument("--num_classes", type=int, default=2, help="Number of classes. Default: 2")    
-    parser.add_argument("--weights_dir", type=str, default="./weights", help="Directory to save model weights. Default: ./weights")
-    args = parser.parse_args()
 
-    model = load_faceboxes(mode=args.mode, img_dim=args.img_dim, num_classes=args.num_classes, resume_net=args.resume_net)
-    model_parameters = [val.cpu().numpy() for _, val in model.state_dict().items()]
 
-    # Create strategy
-    # Create strategy and run server
-    strategy = SaveModelStrategy(
-        model=model,
-        weights_dir=args.weights_dir,
-        fraction_fit=1.0,
-        fraction_evaluate=1.0,
-        min_fit_clients=args.num_clients,
-        min_evaluate_clients=args.num_clients,
-        min_available_clients=args.num_clients,
-        on_fit_config_fn=get_config,
-        on_evaluate_config_fn=get_config,
-        fit_metrics_aggregation_fn=fit_weighted_average,
-        evaluate_metrics_aggregation_fn=evaluate_weighted_average,
-        initial_parameters=fl.common.ndarrays_to_parameters(model_parameters),
-    )
 
-    # Start Flower server for four rounds of federated learning
-    fl.server.start_server(
+
+
+# Load model
+model = load_faceboxes(mode=args.mode, img_dim=args.img_dim, num_classes=args.num_classes, resume_net=args.resume_net)
+ndarrays = get_weights(model)
+parameters = ndarrays_to_parameters(ndarrays)
+
+# Create strategy
+# Create strategy and run server
+strategy = SaveModelStrategy(
+    model=model,
+    weights_dir=args.weights_dir,
+    fraction_fit=1.0,
+    fraction_evaluate=0.0, # disable evaluation
+    min_fit_clients=args.num_clients,
+    min_evaluate_clients=args.num_clients,
+    min_available_clients=args.num_clients,
+    on_fit_config_fn=get_config,
+    on_evaluate_config_fn=get_config,
+    fit_metrics_aggregation_fn=fit_weighted_average,
+    evaluate_metrics_aggregation_fn=evaluate_weighted_average,
+    initial_parameters=parameters,
+)
+
+
+# define config
+config = fl.server.ServerConfig(num_rounds=args.num_rounds)
+
+# Flower ServerApp
+app = fl.server.ServerApp(
+    config=config,
+    strategy=strategy,
+)
+
+
+# Legacy mode
+if __name__ == "__main__":
+    from flwr.server import start_server
+
+    start_server(
         server_address=args.server_address,
-        config=fl.server.ServerConfig(num_rounds=args.num_rounds),
+        config=config,
         strategy=strategy,
     )
-
-
-if __name__ == "__main__":
-    main()
 
